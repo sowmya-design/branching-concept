7548e5ebdc42b8c5da354b23b76e177d6676c87f
cherry <cherry20102024@gmail.com>
2024-11-07 09:27:13 +0530

    modified app.py file

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ client = boto3.client('s3')
 response = client.run_instances(
    ImageId='ami-06b21ccaeff8cd686',
    InstanceType='t2.micro',
-   KeyName='test',
+   KeyName='cherry',
    MaxCount=1,
    MinCount=2
 )
