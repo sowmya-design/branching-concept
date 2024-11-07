6bad368c300a6a99cddb0c526752ba905fd9f174
sowmya-design <srilathamacharla3@gmail.com>
2024-11-07 09:54:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import boto3
 client = boto3.client('s3')
 response = client.run_instances(
    ImageId='ami-06b21ccaeff8cd686',
-   InstanceType='t2.micro',
+   InstanceType='t2.medium',
    KeyName='cherry',
    MaxCount=1,
    MinCount=3
